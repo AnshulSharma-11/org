dba0173d517be6aae05d4c0102c03f700b27ecf4
AnshulSharma-11 <anshuls12120@gmail.com>
2026-08-27 14:24:26 +0530

    Configure

--- frontend/orgplusefe/src/config/apiConfig.js
+++ frontend/orgplusefe/src/config/apiConfig.js
@@ -1,6 +1,7 @@
-const API_URL = process.env.REACT_APP_API_URL || "http://localhost:8080";
+const API_URL = process.env.REACT_APP_API_URLS || "http://localhost:8080";
 
 export let ADMIN_BASE = `${API_URL}/api/v1/admin`;
 
 export let EMPLOYEE_BASE = (employeeId) =>
   `${API_URL}/api/v1/employee/${employeeId}`;
+
